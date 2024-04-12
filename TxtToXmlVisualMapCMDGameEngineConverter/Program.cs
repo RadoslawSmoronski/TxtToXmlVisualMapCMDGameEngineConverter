a586807dd45a0454cf301c6a366867e0d676eeef
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-04-12 15:31:57 +0200

    Created fundamental structure in Main and created Convert and FileHandling methods

--- TxtToXmlVisualMapCMDGameEngineConverter/Program.cs
+++ TxtToXmlVisualMapCMDGameEngineConverter/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Txt to XML VisualMap converter\nfor CMDGameEngine\n\n");
+            Console.Write("File path to file to convert: ");
+
+            string txtPath = Console.ReadLine();
+
+            try
+            {
+                Convert(txtPath);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                Console.WriteLine("\ntxtPath.xml was created succesfully!");
+            }
+
+        }
+
+        static void Convert(string? filePath)
+        {
+           
+        }
+
+        static void FileHandling(string filePath)
+        {
+
         }
     }
 }
